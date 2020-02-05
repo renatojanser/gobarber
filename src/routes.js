97c3e3fa5ab3b74d3d909c92809d22ba59aad617
Renato Janser <renato.janser@gmail.com>
2020-02-05 16:23:46 -0300

    Criei o controller users

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express'
-import User from './app/models/User'
+
+import UserController from './app/controllers/UserController'
 
 const routes = new Router()
 
-routes.get('/', async (req, res) => {
-    const user = await User.create({
-        name: 'Renato Janser',
-        email: 'renato.janser@gmail.com',
-        password_hash: '1238712387',
-    })
-
-    return res.json(user)
-})
+routes.post('/users', UserController.store)
 
 export default routes
