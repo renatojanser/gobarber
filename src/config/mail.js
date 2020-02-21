e450286f1a85f95770b9fc0cc864253f51c352de
Renato Janser <renato.janser@gmail.com>
2020-02-21 16:51:44 -0300

    Configuração do mailtrap

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    secure: false,
+    auth: {
+        user: "75953425c8bcd2",
+        pass: "78520fe32630dc"
+    },
+    default: {
+        from: 'Equipe Gobarber <noreply@gobarber.com>',
+    },
+}
